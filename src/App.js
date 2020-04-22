55321c994cc72b82a1bc720e9490561893d5bd79
Leonardo Oliva Kraciunas <vte_lkraciunas@uolinc.com>
2020-04-22 18:01:55 -0300

    Implementa funcionalidades de listar, adicionar e remover repositorios.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import {
+  getRepositories,
+  createNewRepository,
+  deleteRepositoryById,
+} from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    getRepositories().then(({ data: repositories }) =>
+      setRepositories(repositories)
+    );
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const randomTitle = Date.now();
+
+    const { data: repository } = await createNewRepository({
+      title: randomTitle,
+      url: `http://www.github.com/okraciunas/${randomTitle}`,
+      techs: ["NodeJs", "ReactJS", "JavaScript"],
+    });
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await deleteRepositoryById(id);
+
+    if (response.status !== 204) {
+      throw new Error("Something wrong is not right!");
+    }
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(({ id, title }) => (
+          <li key={id}>
+            {title}
+            <button onClick={() => handleRemoveRepository(id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
+const RESOURCE_REPOSITORIES = "repositories";
+
 const api = axios.create({
   baseURL: "http://localhost:3333",
 });
+
+export const getRepositories = () => {
+  return api.get(RESOURCE_REPOSITORIES);
+};
+
+export const createNewRepository = ({ title, url, techs }) => {
+  return api.post(RESOURCE_REPOSITORIES, { title, url, techs });
+};
+
+export const deleteRepositoryById = (id) => {
+  return api.delete(`${RESOURCE_REPOSITORIES}/${id}`);
+};
 
 export default api;
